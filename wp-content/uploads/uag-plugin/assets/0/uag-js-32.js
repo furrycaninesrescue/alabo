3092b2fe874a60a9e2df6fd488376418ee99e2fe
furrycaninesrescue <furrycaninesrescue.org@gmail.com>
2025-05-13 08:51:24 -0500

    Add files via upload

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-32.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-01974e58' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-01974e58' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-01974e58' );
+});
+ });
